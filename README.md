b50c50eff2464ce9f18bc8407c1dd7bb71ff5961
Luke Watts <luke@luke-watts.com>
2019-06-08 10:55:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Affinity4 Fast Route Middleware
+
+PSR-15 Middleware for Fast Route
+
+## Installation
+
+```bash
+composer require affinity4/fast-route-middleware
+```
+
+## License
+
+MIT License
---
+++ src/FastRouteMiddleware.php
@@ -0,0 +1,102 @@
+<?php
+declare(strict_types = 1);
+
+namespace Affinity4\Middleware\FastRoute;
+
+use FastRoute\Dispatcher;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class FastRouteMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var \FastRoute\Dispatcher
+     */
+    private $router;
+
+    /**
+     * @var string
+     */
+    private $attribute = 'request-handler';
+
+    /**
+     * Constructor
+     * 
+     * Set the Dispatcher instance and optionally the response factory to return the error responses
+     * 
+     * @param \FastRoute\Dispatcher
+     * @param \Psr\Http\Message\ResponseFactoryInterface
+     */
+    public function __construct(Dispatcher $router, ResponseFactoryInterface $HttpFactory)
+    {
+        $this->router = $router;
+        $this->HttpFactory = $HttpFactory;
+    }
+
+    /**
+     * Attribute
+     * 
+     * Set the attribute name to store handler reference
+     * 
+     * @param string $attribute
+     * 
+     * @return self
+     */
+    public function attribute(string $attribute): self
+    {
+        $this->attribute = $attribute;
+
+        return $this;
+    }
+
+    /**
+     * Process
+     * 
+     * Process a server request and return a response
+     * 
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Server\RequestHandlerInterface $handler
+     * 
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $route = $this->router->dispatch($request->getMethod(), rawurldecode($request->getUri()->getPath()));
+
+        if ($route[0] === Dispatcher::NOT_FOUND) {
+            return $this->HttpFactory->createResponse(404);
+        }
+
+        if ($route[0] === Dispatcher::METHOD_NOT_ALLOWED) {
+            return $this->HttpFactory->createResponse(405)->withHeader('Allow', implode(', ', $route[1]));
+        }
+
+        foreach ($route[2] as $name => $value) {
+            $request = $request->withAttribute($name, $value);
+        }
+
+        $request = $this->setHandler($request, $route[1]);
+
+        $Response = $handler->handle($request);
+        
+        return $Response->withBody($this->HttpFactory->createStream($Response->getBody() . $route[1]($request)->getBody()));
+    }
+
+    /**
+     * Set Handler
+     * 
+     * Set the handler reference on the request
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param mixed $handler
+     * 
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    protected function setHandler(ServerRequestInterface $request, $handler): ServerRequestInterface
+    {
+        return $request->withAttribute($this->attribute, $handler);
+    }
+}
